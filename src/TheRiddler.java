7033b6f9873f34a2385f4cf17f8d4d7c116279b3
Nolan Platt <nolanplattgithub@gmail.com>
2017-11-14 19:24:34 -0800

    The Riddler

--- src/TheRiddler.java
+++ src/TheRiddler.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
@@ -8,9 +10,13 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+int score = o;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+JOptionPane.showMessageDialog("What is greater than God,
+more evil than the devil,
+the poor have it,
+the rich need it,
+and if you eat it, you'll die?");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
 
 		// 5. Otherwise, say "wrong" and tell them the answer
